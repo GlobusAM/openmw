d3a71ac096076636e4d14ff7258c78bbdaa7e6fb
Amur <peskrish228@gmail.com>
2026-03-26 13:10:13 +0300

    Update objects.hpp

--- apps/openmw/mwrender/objects.hpp
+++ apps/openmw/mwrender/objects.hpp
@@ -26,6 +26,7 @@ namespace MWWorld
 
 namespace SceneUtil
 {
+    class OcclusionCuller;
     class UnrefQueue;
 }
 
@@ -89,7 +90,21 @@ namespace MWRender
         /// Updates containing cell for object rendering data
         void updatePtr(const MWWorld::Ptr& old, const MWWorld::Ptr& cur);
 
+        void setOcclusionCuller(SceneUtil::OcclusionCuller* culler, float occluderMinRadius, float occluderMaxRadius,
+            float occluderShrinkFactor, int occluderMeshResolution, int occluderMaxMeshResolution,
+            float occluderInsideThreshold, float occluderMaxDistance, bool enableStaticOccluders);
+
     private:
+        SceneUtil::OcclusionCuller* mOcclusionCuller = nullptr;
+        float mOccluderMinRadius = 300.0f;
+        float mOccluderMaxRadius = 5000.0f;
+        float mOccluderShrinkFactor = 0.5f;
+        int mOccluderMeshResolution = 8;
+        int mOccluderMaxMeshResolution = 24;
+        float mOccluderInsideThreshold = 1.0f;
+        float mOccluderMaxDistance = 6144.0f;
+        bool mEnableStaticOccluders = true;
+
         void operator=(const Objects&);
         Objects(const Objects&);
     };
